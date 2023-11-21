890387534789e293af7649581ce0a48df793017e
Ashar Ali <asher.ali@mmcgbl.com>
2023-11-21 19:12:39 +0500

    chair svg trial

--- demo/src/catalog/mycatalog.js
+++ demo/src/catalog/mycatalog.js
@@ -10,7 +10,7 @@ import * as Items from './items/**/planner-element.jsx';
 for( let x in Areas ) catalog.registerElement( Areas[x] );
 for( let x in Lines ) catalog.registerElement( Lines[x] );
 for( let x in Holes ) catalog.registerElement( Holes[x] );
-// for( let x in Items ) catalog.registerElement( Items[x] );
+for( let x in Items ) catalog.registerElement( Items[x] );
 
 catalog.registerCategory('windows', 'Windows', [Holes.window, Holes.sashWindow, Holes.venetianBlindWindow, Holes.windowCurtain] );
 catalog.registerCategory('doors', 'Doors', [Holes.door, Holes.doorDouble, Holes.panicDoor, Holes.panicDoorDouble, Holes.slidingDoor] );
